a24ab580a4bc07502f8c9d6d503aeb7023fc9034
ChaosCantrip <dev@chaoscantrip.com>
2025-01-09 11:51:57 +0000

    Adjusted stylings

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import styles from "./CardPage.module.css";
 export default function CardPage() {
     return (
         <div className={styles.card}>
-            <h1>James Donald</h1>
-            <h2>@chaoscantrip</h2>
+            <section className={styles.identity}>
+                <h1 className={styles.name}>James Donald</h1>
+                <h2 className={styles.tag}>@chaoscantrip</h2>
+            </section>
         </div>
     )
 }
