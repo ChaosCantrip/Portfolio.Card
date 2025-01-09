c9c37a8a77c3e3db6e03136afbf4281cfad9ccc1
ChaosCantrip <dev@chaoscantrip.com>
2025-01-09 12:05:17 +0000

    Added first LinkBar

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import styles from "./CardPage.module.css";
 
 export default function CardPage() {
@@ -7,6 +9,18 @@ export default function CardPage() {
                 <h1 className={styles.name}>James Donald</h1>
                 <h2 className={styles.tag}>@chaoscantrip</h2>
             </section>
+            <section className={styles.links}>
+                <LinkBar text="GitHub" styleName={styles.github} icon={faGithub} href="https://github.com/ChaosCantrip" />
+            </section>
         </div>
+    )
+}
+
+function LinkBar({ text, styleName, icon, href }){
+    return (
+        <a href={href} className={`${styles.link_bar} ${styleName}`}>
+            <FontAwesomeIcon icon={icon} className={styles.link_bar_icon}/>
+            <p className={styles.link_bar_text}>{text}</p>
+        </a>
     )
 }
