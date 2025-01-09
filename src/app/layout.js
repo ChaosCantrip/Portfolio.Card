b9ee9b3c81d86e025d01becaeeea590031429ee3
ChaosCantrip <dev@chaoscantrip.com>
2025-01-09 12:05:10 +0000

    FontAwesome Setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import "@/styles/global/master.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 export default function RootLayout({ children }){
     return (
         <html>
