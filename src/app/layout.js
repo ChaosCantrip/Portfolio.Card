6afc95888760757863de9c2580d54e266a2ff9ac
ChaosCantrip <dev@chaoscantrip.com>
2025-01-09 11:44:02 +0000

    Added basic card stylings

---
+++ src/app/layout.js
@@ -0,0 +1,13 @@
+import "@/styles/global/master.css";
+
+export default function RootLayout({ children }){
+    return (
+        <html>
+            <body>
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import styles from "./CardPage.module.css";
+
+export default function CardPage() {
+    return (
+        <div className={styles.card}>
+            <h1>James Donald</h1>
+            <h2>@chaoscantrip</h2>
+        </div>
+    )
+}
