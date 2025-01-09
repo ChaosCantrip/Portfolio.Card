390afe3245f2827f0e3a4c58f80b8b178754ea00
ChaosCantrip <dev@chaoscantrip.com>
2025-01-09 14:24:14 +0000

    Added Twitter and LinkedIn

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faXTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import styles from "./CardPage.module.css";
 
 export default function CardPage() {
@@ -11,6 +11,8 @@ export default function CardPage() {
             </section>
             <section className={styles.links}>
                 <LinkBar text="GitHub" styleName={styles.github} icon={faGithub} href="https://github.com/ChaosCantrip" />
+                <LinkBar text="X (Twitter)" styleName={styles.twitter} icon={faXTwitter} href="https://twitter.com/chaoscantrip" />
+                <LinkBar text="LinkedIn" styleName={styles.linkedin} icon={faLinkedin} href="https://www.linkedin.com/in/chaoscantrip/" />
             </section>
         </div>
     )
@@ -18,7 +20,7 @@ export default function CardPage() {
 
 function LinkBar({ text, styleName, icon, href }){
     return (
-        <a href={href} className={`${styles.link_bar} ${styleName}`}>
+        <a href={href} className={`${styles.link_bar} ${styleName}`} target="_blank">
             <FontAwesomeIcon icon={icon} className={styles.link_bar_icon}/>
             <p className={styles.link_bar_text}>{text}</p>
         </a>
