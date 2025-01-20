d114b9ba3737468567ea5cb009b84b79b93b79fe
ChaosCantrip <dev@chaoscantrip.com>
2025-01-20 05:20:30 +0000

    Changed to use image on Google Storage Bucket

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "storage.googleapis.com",
+                port: "",
+                pathname: "/bucket_chaoscantrip_portfolio_images/**",
+                search: ""
+            }
+        ]
+    }
+};
 
 export default nextConfig;
